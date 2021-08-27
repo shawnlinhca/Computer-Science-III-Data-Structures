5b073e9689459d33819f0a2afb9282712a5e15f4
school <shawn.lin.hca@gmail.com>
2021-08-27 13:42:11 -0500

    8/27 school

--- Spiral/Spiral.java
+++ Spiral/Spiral.java
@@ -1,5 +1,52 @@
-public class Spiral{
-    public Spiral(int i){
+public class Spiral {
+
+    int m[][];
+
+    int r, c, filled, FILLED, dir;
+
+    public Spiral(String s) {
+
+        int length = Integer.parseInt(s);
+
+        m = new int[length][length];
+        r = 0;
+        c = 0;
+        filled = 0;
+        FILLED = length * length;
+
+        while (filled != FILLED) {
+            m[r][c] = ++count;
+
+
+
+        }
 
     }
+
+    public void changeDir(){
+        if (dir == 1) {
+            if(r> m.length)
+        }
+        if (dir == 2){
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        for (int[] e : m) {
+
+            for (int a : e) {
+                s.append(a + '\t');
+            }
+
+            s.append('\n');
+
+        }
+
+        return s.toString();
+    }
+
 }
--- Spiral/SpiralRunner.java
+++ Spiral/SpiralRunner.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 public class SpiralRunner{
-    public static void main(String []args){
+    public static void main(String []args) throws Exception {
+        Scanner input = new Scanner( new File("sprial.txt") );
+
+        while (input.hasNextLine()) {
+            System.out.println( new Spiral( input.nextLine() ) );
+        }
 
     }
 }

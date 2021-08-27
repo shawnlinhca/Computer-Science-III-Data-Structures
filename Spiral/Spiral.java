8a996a66853dd4658172abacd001ebaf64205f2d
shawnlinhca <71190839+shawnlinhca@users.noreply.github.com>
2021-08-26 21:23:40 -0500

    8/26

--- Spiral/Spiral.java
+++ Spiral/Spiral.java
@@ -1,5 +1,9 @@
 public class Spiral{
-    public Spiral(int i){
+    public Spiral(int input){
+        int[][] arr = new arr[input][input];
+        int count = 0;
+        int(i = 0;i<input;i++){
 
+        }
     }
 }
